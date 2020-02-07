654a9de58e5c1eec2b5c8ea9dc36b45d28a36656
Apoorva Shetty <apoorvashetty@Apoorvas-MacBook-Pro.local>
2020-02-06 18:33:05 -0800

    Adding an init file

--- zodiac/zodiac.py
+++ zodiac/zodiac.py
@@ -72,6 +72,62 @@ class Zodiac:
             self.metrics.push(custom_func)
             self.custom_func = True
 
+    def __in_windows(self,x1, x2, y1, y2, x, y):
+        """
+
+        :param x1:
+        :param x2:
+        :param y1:
+        :param y2:
+        :param x:
+        :param y:
+        :return:
+        """
+        if (x >= x1 and x < x2) and (y >= y1 and y < y2):
+            return True
+        return False
+
+    def __gen_density_matrix(self):
+        """
+
+        :return:
+        """
+        count = len(self.test_data)
+        den_map = []
+        for x in range(len(self.x_axis) - 1):
+            for y in range(len(self.y_axis) - 1):
+                dmrow = [self.x_axis[x], self.x_axis[x + 1], self.y_axis[y], self.y_axis[y + 1]]
+                results = []
+                preds = []
+                density = 0
+                for i in self.test_data.values:
+                    if self.__in_windows(self.x_axis[x], self.x_axis[x + 1], self.y_axis[y], self.y_axis[y + 1], i[0], i[1]):
+                        results.append(i[3])
+                        preds.append(i[2])
+                        density = density + 1
+                dmrow.append(density)
+                dmrow.append(density/count)
+                if density != 0:
+                    if not self.custom_func:
+                        for function in self.metrics:
+                            if function == "f1":
+                                f1 = f1_score(results, preds, average='micro')
+                                dmrow.append(f1)
+                            elif function == "accuracy":
+                                accuracy = accuracy_score(results, preds)
+                                dmrow.append(accuracy)
+                            elif function == "recall":
+                                recall = recall_score(results, preds, average='micro')
+                                dmrow.append(recall)
+                            elif function == "precision":
+                                precision = precision_score(results, preds, average='micro')
+                                dmrow.append(precision)
+                    den_map.append(dmrow)
+
+        self.density_map = pd.DataFrame(data=den_map,
+                          columns=self.columns)
+
+
     def split_manual_grid(self,h=-1):
         """
 
@@ -113,63 +169,11 @@ class Zodiac:
             for i in range(1, numDivY + 1):
                 self.y_axis.append((i * h) + miny)
 
-        __gen_density_matrix()
+        self.__gen_density_matrix()
+
+
         
 
-    def __in_windows(self,x1, x2, y1, y2, x, y):
-        """
-
-        :param x1:
-        :param x2:
-        :param y1:
-        :param y2:
-        :param x:
-        :param y:
-        :return:
-        """
-        if (x >= x1 and x < x2) and (y >= y1 and y < y2):
-            return True
-        return False
-
-    def __gen_density_matrix(self):
-        """
-
-        :return:
-        """
-        count = len(self.test_data)
-        den_map = []
-        for x in range(len(self.x_axis) - 1):
-            for y in range(len(self.y_axis) - 1):
-                dmrow = [self.x_axis[x], self.x_axis[x + 1], self.y_axis[y], self.y_axis[y + 1]]
-                results = []
-                preds = []
-                density = 0
-                for i in self.test_data.values:
-                    if __in_windows(self.x_axis[x], self.x_axis[x + 1], self.y_axis[y], self.y_axis[y + 1], i[0], i[1]):
-                        results.append(i[3])
-                        preds.append(i[2])
-                        density = density + 1
-                dmrow.append(density)
-                dmrow.append(density/count)
-                if density != 0:
-                    if not self.custom_func:
-                        for function in self.metrics:
-                            if function == "f1":
-                                f1 = f1_score(results, preds, average='micro')
-                                dmrow.append(f1)
-                            elif function == "accuracy":
-                                accuracy = accuracy_score(results, preds)
-                                dmrow.append(accuracy)
-                            elif function == "recall":
-                                recall = recall_score(results, preds, average='micro')
-                                dmrow.append(recall)
-                            elif function == "precision":
-                                precision = precision_score(results, preds, average='micro')
-                                dmrow.append(precision)
-                    den_map.append(dmrow)
-
-        self.density_map = pd.DataFrame(data=den_map,
-                          columns=self.columns)
 
 
 
